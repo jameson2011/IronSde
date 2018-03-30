4f972018428aebb3a1389e243776b12e9d618bf5
jameson2011 <jameson2011@users.noreply.github.com>
2018-03-30 18:26:23 +0100

    programmer fail: fix assemblyinfo internalsvisibleto

--- src/IronSde.Universe/AssemblyInfo.fs
+++ src/IronSde.Universe/AssemblyInfo.fs
@@ -2,12 +2,15 @@
 
 open System.Reflection
 open System.Runtime.InteropServices
+open System.Runtime.CompilerServices
 
 [<assembly: AssemblyTitle("IronSde.Universe")>]
 [<assembly: AssemblyDescription("")>]
 
 [<assembly: ComVisible(false)>]
 [<assembly: Guid("bcb4086e-ccd1-492b-a1b4-923e71acde01")>]
+
+[<assembly: InternalsVisibleTo("IronSde")>]
 
 do
     ()
