4289abc2859632d934a3481a41c73a3cbbe69cf0
jameson2011 <jameson2011@users.noreply.github.com>
2018-04-26 20:59:47 +0100

    remove redundant types

--- src/Shared/TypeEntities.fs
+++ src/Shared/TypeEntities.fs
@@ -1,11 +1,5 @@
 namespace IronSde.Types
 
-// TODO: 
-type internal CategoryData =  { id: int; }
-
-// TODO: 
-type internal GroupData =  { id: int; categoryId: int; }
-
 type internal AttributeTypeData = { id: int; categoryId: int option; name: string; defaultValue: float option; }
 
 type internal ItemTypeAttributeData = { attributeId: int; value: float option; }
